da2afda08c9c902f69941f47ea1a47ff3bcabf96
jasony123123 <50646589+jasony123123@users.noreply.github.com>
2022-03-25 15:39:34 -0400

    Update visualizer.py

--- fedml-metrics/visualizer.py
+++ fedml-metrics/visualizer.py
@@ -1,13 +1,19 @@
 import torch
 import matplotlib.pyplot as plt
 
-name = 'fedml_classic'
-metrics = torch.load('{}.metrics'.format(name))
-plt.plot([x['train_acc1'] for x in metrics], label='train_acc1')
-plt.plot([x['test_acc1'] for x in metrics], label='test_acc1')
-plt.title('{}_acc1'.format(name))
-plt.show()
-plt.plot([x['train_loss'] for x in metrics], label='train_loss')
-plt.plot([x['test_loss'] for x in metrics], label='test_loss')
-plt.title('{}_loss'.format(name))
-plt.show()
+names = [
+         'fedml_fed1',
+         'fedml_fed2',
+         'fedml_fed3',
+         'fedml_syncSgdSmall'
+]
+for name in names:
+    metrics = torch.load('{}.metrics'.format(name))
+    plt.plot([x['train_acc1'] for x in metrics], label='train_acc1')
+    plt.plot([x['test_acc1'] for x in metrics], label='test_acc1')
+    plt.title('{}_acc1'.format(name))
+    plt.show()
+    plt.plot([x['train_loss'] for x in metrics], label='train_loss')
+    plt.plot([x['test_loss'] for x in metrics], label='test_loss')
+    plt.title('{}_loss'.format(name))
+    plt.show()
