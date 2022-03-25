c0d13327be06a67f9039f7c6207a124817b5189c
Jason Yuan <jasony123123@gmail.com>
2022-03-25 03:07:37 +0000

    update

---
+++ fedml_runscript.sh
@@ -0,0 +1,1 @@
+python fedml_fed1.py ; git add . ; git commit -m "update" ; git push ; python fedml_fed2.py ; git add . ; git commit -m "update" ; git push ; python fedml_syncSgdSmall.py ; git add . ; git commit -m "update" ; git push ; python fedml_classic.py ; git add . ; git commit -m "update" ; git push ;
